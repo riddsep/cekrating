9d232b061810da0d940ba41d86dd5f7f432c8cf4
riddsep <ridoseptiawan00@gmail.com>
2024-12-16 18:10:22 +0700

    minor changes

--- src/components/WatchedSummary.jsx
+++ src/components/WatchedSummary.jsx
@@ -1,32 +1,32 @@
-import { average } from "../utils/average.js";
+// import { average } from "../utils/average.js";
 
-export function WatchedSummary({ watched }) {
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.Runtime));
-  return (
-    <div className="summary">
-      <h2>Movies you watched</h2>
-      <div>
-        <p>
-          <span>#️⃣</span>
-          <span className="badge">
-            {watched.length} <span>movies</span>
-          </span>
-        </p>
-        <p>
-          <span>⭐️</span>
-          <span>{avgImdbRating.toFixed(2)}</span>
-        </p>
-        <p>
-          <span>🌟</span>
-          <span>{avgUserRating.toFixed(2)}</span>
-        </p>
-        <p>
-          <span>⏳</span>
-          <span>{avgRuntime.toFixed(2)} min</span>
-        </p>
-      </div>
-    </div>
-  );
-}
+// export function WatchedSummary({ watched }) {
+//   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+//   const avgUserRating = average(watched.map((movie) => movie.userRating));
+//   const avgRuntime = average(watched.map((movie) => movie.Runtime));
+//   return (
+//     <div className="summary">
+//       <h2>Movies you watched</h2>
+//       <div>
+//         <p>
+//           <span>#️⃣</span>
+//           <span className="badge">
+//             {watched.length} <span>movies</span>
+//           </span>
+//         </p>
+//         <p>
+//           <span>⭐️</span>
+//           <span>{avgImdbRating.toFixed(2)}</span>
+//         </p>
+//         <p>
+//           <span>🌟</span>
+//           <span>{avgUserRating.toFixed(2)}</span>
+//         </p>
+//         <p>
+//           <span>⏳</span>
+//           <span>{avgRuntime.toFixed(2)} min</span>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
